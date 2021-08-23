493394c698684b48f6ee7bddf2624866b11cc6f0
Rinkton <bystrova1979inbox.ru@gmail.com>
2021-08-23 18:03:37 +0300

    Проекты созданы, настроены и пр.

---
+++ NewRpgGame/Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using Game;
+
+namespace Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            Tested tested = new Tested();
+            Assert.Equal(5, tested.GetFive());
+        }
+    }
+}
