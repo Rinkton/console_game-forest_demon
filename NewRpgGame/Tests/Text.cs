3f8fed152684754cb41f24c46a4660ba11e5fd42
Rinkton <bystrova1979inbox.ru@gmail.com>
2021-08-23 20:43:28 +0300

    Сделал получение string-ов из ресурсов

---
+++ NewRpgGame/Tests/Text.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using Game;
+
+namespace Tests
+{
+    public class Text
+    {
+        [Theory]
+        [InlineData("test", "Тест")]
+        public void Test(string given1, string expected)
+        {
+            var actual = Game.Text.GetStringByResourceName(given1);
+            Assert.Equal(actual, expected);
+        }
+    }
+}
