63f0a63d15822f15532501a0a2fa9aa9d7eae862
Rinkton <bystrova1979inbox.ru@gmail.com>
2021-08-25 21:27:11 +0300

    Соединил оружия, броню(и ожерелья) в Items директорию и воссоздал им каркас

---
+++ NewRpgGame/Game/Items/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Items
+{
+    abstract class Item
+    {
+        /// <summary>
+        /// Событие, которое происходит при надевании(создании экземпляра) предмета
+        /// </summary>
+        public readonly GameEvents.GameEvent Wearing;
+
+        /// <summary>
+        /// Событие, которое происходит при использовании предмета 
+        /// (Например удар по игроку вызывает событие у брони, когда же удар по врагу вызывает событие у оружия)
+        /// </summary>
+        public readonly GameEvents.GameEvent Using;
+
+        public Item()
+        {
+            Wearing.Run();
+        }
+
+        //TODO: Сделать настоящий Event чтобы вызывать Using
+    }
+}
---
+++ NewRpgGame/Game/Items/Necklets/Necklet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Items.Necklets
+{
+    class Necklet : Item
+    {
+        //Ну, от обычного предмета ожерелья ничем не отличаются, но ради красивой архитектуры...
+    }
+}
