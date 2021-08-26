f55aa03cd02bb149495fb6f4a3037f1fc73177d2
Rinkton <bystrova1979inbox.ru@gmail.com>
2021-08-26 23:16:39 +0300

    Добавил базовый класс врагов

---
+++ NewRpgGame/Game/Enemies/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Enemies
+{
+    //TODO: И все же надо сделать все такие вот класса типа Enemy абстрактными
+    class Enemy
+    {
+        public string Name;
+        public string Description;
+        public int Health;
+        public int Damage;
+        public int Defense;
+        /// <summary>
+        /// Среднее кол-во монет, коеи можно получить, победив этого врага
+        /// </summary>
+        public int AverageLoot;
+
+        //Шансы в процентах на какой-либо определённый вид удара:
+        public int MissPercentChance;
+        public int WeakPercentChance;
+        public int NormalPercentChance;
+        public int StrongPercentChance;
+    }
+}
