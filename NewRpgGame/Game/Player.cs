421c08f2a49be0cec4e1fe46250959fcc156e6dc
Rinkton <bystrova1979inbox.ru@gmail.com>
2021-08-27 13:16:01 +0300

    Добавил характеристики для игрока(сделал каркас)

--- NewRpgGame/Game/Player.cs
+++ NewRpgGame/Game/Player.cs
@@ -8,5 +8,21 @@ namespace Game
 {
     class Player
     {
+        #region Отображаемые характеристики
+        public int MaxHealth;
+        public int CurrentHealth;
+        public Items.Weapons.Weapon Weapon;
+        public Items.Armors.Armor Armor;
+        public Items.Necklets.Necklet Necklet;
+        public int PunishmentPercent;
+        public int Money;
+        public int HealingPotionCount;
+        #endregion
+
+        //Шансы в процентах на какой-либо определённый вид удара:
+        public int MissPercentChance;
+        public int WeakPercentChance;
+        public int NormalPercentChance;
+        public int StrongPercentChance;
     }
 }
